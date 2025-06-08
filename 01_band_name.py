b91b87e35b08216d6e81dd77b15e3030162c8ca6
Terezie Janíková <tereziejanikova@gmail.com>
2025-06-08 16:25:35 +0200

    Initial commit

---
+++ 01_band_name.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+City = input("What's the name of the city you grew up in?\n")
+Pet_name = input("What's your pet's name?\n")
+print("Your band name could be: " + City + " " + Pet_name)
